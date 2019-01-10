9e490e6f9e98b2fb31ebcd1edf98262decc8a065
phanthanhgit <doanphanthanh1305@gmail.com>
2019-01-10 21:16:32 +0700

    1012019

---
+++ Olympic CP 2019 Round 2/ProblemA.py
@@ -0,0 +1,6 @@
+__author__ = 'Doan Phan Thanh'
+'''
+Olympic CP 2019 Round 2
+Link problem:
+http://oj.hueuni.edu.vn/contest/192/problem/652/details
+'''
--- ThreeDigits_199.py
+++ ThreeDigits_199.py
@@ -1,18 +1,26 @@
-import math
-from sys import stdin, stdout
+__author__ = 'Doan Phan Thanh'
+'''
+Project Basic Python
+Machine Learning
+https://github.com/phanthanhgit/oj.hueuni
+'''
 
-def main():
-    t = int(raw_input())
+#from sys import stdin
+
+def solve(t):
     for _ in range(t):
-        st = stdin.readline()
+        st = raw_input()
         lst = []
         ans = 0
         for i in range(len(st) - 3):
-            if not st[i] == "0":
+            if not st[i] == '0':
                 s = 0
                 s = int(st[i])*100 + int(st[i+1])*10 + int(st[i+2])
                 if not s in lst:
                     lst.append(s)
                     ans += 1
         print(ans)
-main()
+
+if __name__ == "__main__":
+    t = int(raw_input())
+    solve(t)
