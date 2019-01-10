1bb99eaa459a0dcb156e2e2c6c1f8f805b32a93c
phanthanhgit <doanphanthanh1305@gmail.com>
2019-01-10 17:06:56 +0700

    1012019

--- ThreeDigits_199.py
+++ ThreeDigits_199.py
@@ -1,7 +1,24 @@
 import math
 from sys import stdin, stdout
 
+def check(a, k):
+    for x in a:
+        if x == k:
+            return False
+    return True
+
 def main():
-    t = int(input())
-    
+    t = int(raw_input())
+    for _ in range(t):
+        st = stdin.readline()
+        lst = []
+        ans = 0
+        for i in range(len(st) - 3):
+            if not st[i] == "0":
+                s = 0
+                s = int(st[i])*100 + int(st[i+1])*10 + int(st[i+2])
+                if check(lst, s):
+                    lst.append(s)
+                    ans += 1
+        print(ans)
 main()
